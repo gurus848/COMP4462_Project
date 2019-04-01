cbc7404478ccbfe7a8dec0f963189fe4b9f7326c
Guru Sarjith Senthil <gurus848@gmail.com>
2019-04-01 22:12:58 +0800

    started to work on themeriver

--- Task_1/Viz_2_Themeriver/README.md
+++ Task_1/Viz_2_Themeriver/README.md
@@ -4,3 +4,4 @@
 
 Depicts growing and shrinking popularity of the song in different countries over time
 
+Observable notebook of Vega Themeriver: https://observablehq.com/@gurus848/task-2-visualization-1-themeriver
---
+++ Task_1/Viz_2_Themeriver/process_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+rankings_file_path = "../../data/Daily_Ranking.csv"
+output_folder_path = "output_song_data"
+
+df = pd.read_csv(rankings_file_path);
+particular_song_name = "Shape of You"
+
+song_d = df[df["Track Name"] == particular_song_name]
+song_d = song_d[song_d["Position"] < 50]
+song_d["Inv_Pos"] = 50 - song_d["Position"]
+new_song_d = song_d.drop(columns=['Track Name', 'Artist', 'URL'])
+new_song_d.to_csv("{}/test.csv".format(output_folder_path),index=False)
